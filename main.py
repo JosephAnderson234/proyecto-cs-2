65b9596df3feb0deb7927dcf24d04cfc266aa301
JosephAnderson234 <joseph.cose@utec.edu.pe>
2024-07-06 17:35:07 -0500

    Starting

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from sklearn import datasets
+import pandas as pd
+
 ##Pregunta a:
 """Manipular el dataset digits de sklearn para generar una matriz de 8x8 con la
 imagen promedio de cada uno de los 10 dígitos disponibles en este dataset. Puede
 generarlas todas juntas, o generarlas en base a un menú, o generarlas en base al
 número que el usuario ingrese por teclado"""
+
